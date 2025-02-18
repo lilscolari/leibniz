aca159c3e07f94267d2c0adb5d6a135621be188c
Cameron Scolari <cameronscolari@outlook.com>
2025-02-18 15:16:25 -0800

    Setup project? Still need to update README, write parser, write ohm grammar, and write parser tests

---
+++ test/parser.test.js
@@ -0,0 +1,26 @@
+import { describe, it } from "node:test"
+import assert from "node:assert/strict"
+import parse from "../src/parser.js"
+
+// Programs expected to be syntactically correct
+const syntaxChecks = [
+  
+]
+
+// Programs with syntax errors that the parser will detect
+const syntaxErrors = [
+  
+]
+
+describe("The parser", () => {
+  for (const [scenario, source] of syntaxChecks) {
+    it(`matches ${scenario}`, () => {
+      assert(parse(source).succeeded())
+    })
+  }
+  for (const [scenario, source, errorMessagePattern] of syntaxErrors) {
+    it(`throws on ${scenario}`, () => {
+      assert.throws(() => parse(source), errorMessagePattern)
+    })
+  }
+})
