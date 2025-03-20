5ff81467b1f6ba37e6a89a06aabb920eba09169e
Cameron <cameronscolari@outlook.com>
2025-03-20 14:29:59 -0700

    started analyze and core

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,3 +1,38 @@
+import * as core from "./core.js";
+
 export default function analyze(match) {
-    throw new Error("Not implemented")
+  const grammar = match.matcher.grammar;
+
+  class Context {
+    constructor(parent = null) {
+      this.locals = new Map();
+      this.parent = parent;
+    }
+    add(name, entity) {
+      this.locals.set(name, entity);
+    }
+    has(name) {
+      return this.locals.has(name);
+    }
+    lookup(name) {
+      return this.locals.get(name) ?? (this.parent && this.parent.lookup(name));
+    }
+    newChildContext() {
+      return new Context(this);
+    }
+  }
+
+  let context = new Context();
+  const target = [];
+    
+
+
+  const analyzer = grammar.createSemantics().addOperation("analyze", {
+    Program(statements) {
+      return core.program(statements.children.map((s) => s.analyze()));
+    },
+
+  });
+  return analyzer(match).analyze();
+
 }
---
+++ src/core.js
@@ -0,0 +1,6 @@
+export function program(statements) {
+  return {
+    kind: "Program",
+    statements,
+  };
+}
