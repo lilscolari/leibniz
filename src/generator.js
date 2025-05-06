68a2fcfd3f93a6700f72e73cc3965d2be3d02e0d
Cameron <cameronscolari@outlook.com>
2025-05-06 14:29:04 -0700

    JUST YELLOW TESTS

--- src/generator.js
+++ src/generator.js
@@ -36,9 +36,10 @@ export default function generate(program) {
       output.push(`while (${gen(s.test)}) {`)
       if (s.body.statements) {
         s.body.statements.forEach(gen);
-      } else {
-        s.body.forEach(gen);
-      }
+      } 
+      // else {
+      //   s.body.forEach(gen);
+      // }
       output.push("}")
     },
     Variable(v) {
@@ -65,18 +66,20 @@ export default function generate(program) {
     IfStatement(s) {
       output.push(`if (${gen(s.test)}) {`);
       s.consequent.statements.forEach(gen);
-      if (s.alternate?.kind?.endsWith?.("IfStatement")) {
-        output.push("} else");
-        gen(s.alternate);
-      } else {
-        if (s.alternate != null) {
-          output.push("} else {");
-          s.alternate.statements.forEach(gen);
-          output.push("}");
-        } else {
-          output.push("}")
-        }
-      }
+      output.push(`}`);
+      // if (s.alternate?.kind?.endsWith?.("IfStatement")) {
+      //   console.log("IN HERE")
+      //   output.push("} else");
+      //   gen(s.alternate);
+      // } else {
+      //   if (s.alternate != null) {
+      //     output.push("} else {");
+      //     s.alternate.statements.forEach(gen);
+      //     output.push("}");
+      //   } else {
+      //     output.push("}")
+      //   }
+      // }
     },
     BinaryExpression(e) {
       const op = { "==": "===", "!=": "!==" }[e.op] ?? e.op;
@@ -237,11 +240,12 @@ export default function generate(program) {
     
     ForLoopStatement(s) {
       output.push(`for (let ${gen(s.loopVar)} = ${gen(s.start)}; ${gen(s.loopVar)} < ${gen(s.stop)}; ${gen(s.loopVar)} += ${gen(s.step)}) {`);
-      if (s.body.statements) {
-        s.body.statements.forEach(gen);
-      } else {
-        s.body.forEach(gen);
-      }
+      // if (s.body.statements) {
+      //   s.body.statements.forEach(gen);
+      // } 
+      //else {
+      s.body.forEach(gen);
+      //}
       output.push("}");
     },
     
